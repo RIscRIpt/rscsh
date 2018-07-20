63d90916aea5480cfe2b21b07dffb14fe0c9bb34
RIscRIpt <richard@riscript.com>
2018-07-20 20:05:42 +0300

    handle exceptions in main

--- rscsh/main.cpp
+++ rscsh/main.cpp
@@ -3,5 +3,16 @@
 #include "Application.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    return Application(hInstance).run();
+    try {
+        return Application(hInstance).run();
+    } catch (std::system_error const &e) {
+        MessageBoxA(HWND_DESKTOP, e.what(), "System Error", MB_ICONERROR);
+        return e.code().value();
+    } catch (std::runtime_error const &e) {
+        MessageBoxA(HWND_DESKTOP, e.what(), "Runtime Error", MB_ICONERROR);
+        return -2;
+    } catch (std::exception const &e) {
+        MessageBoxA(HWND_DESKTOP, e.what(), "Error", MB_ICONERROR);
+        return -1;
+    }
 }
