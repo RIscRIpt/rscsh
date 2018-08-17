fe394c57fc4909235b8855ac73450ec4e26ba0c7
RIscRIpt <richard@riscript.com>
2018-08-17 16:47:02 +0300

    fix mistake in main shell command description

--- rscsh/MainShell_commands.h
+++ rscsh/MainShell_commands.h
@@ -1,3 +1,3 @@
 X( L"help",                  help,                  L"\r\n\t-- Display this help text." )
 X( L"exit",                  exit,                  L"\r\n\t-- Exit the program." )
-X( L"version",               version,               L"\r\n\t-- Print the version rscsh." )
+X( L"version",               version,               L"\r\n\t-- Print the version of rscsh." )
